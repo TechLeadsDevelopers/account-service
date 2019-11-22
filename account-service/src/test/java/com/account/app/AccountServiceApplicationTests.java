562cee0789615a9adcc04b8224b23103568410a1
ANUPOJM <ANUPOJM@CTNAVW7LRGIC017.us164.corpintra.net>
2019-11-22 12:17:42 +0530

    Added packages in account-service

---
+++ account-service/src/test/java/com/account/app/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
